428367196a3bcea65f83c4f27dd182653ca1070e
dmg81714 <dmg81714@gmail.com>
2023-01-23 16:45:41 +0100

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -33,7 +33,7 @@ def get_item(key, dynamodb=None):
         )
 
     except ClientError as e:
-    #     print(e.response['Error']['Message'])
+        print(e.response['Error']['Message'])
     else:
         print('Result getItem:'+str(result))
         if 'Item' in result:
@@ -68,7 +68,7 @@ def put_item(text, dynamodb=None):
         }
 
     except ClientError as e:
-    #     print(e.response['Error']['Message'])
+        print(e.response['Error']['Message'])
     else:
         return response
 
@@ -97,7 +97,7 @@ def update_item(key, text, checked, dynamodb=None):
         )
 
     except ClientError as e:
-    #     print(e.response['Error']['Message'])
+        print(e.response['Error']['Message'])
     else:
         return result['Attributes']
 
@@ -113,7 +113,7 @@ def delete_item(key, dynamodb=None):
         )
 
     except ClientError as e:
-    #    print(e.response['Error']['Message'])
+       print(e.response['Error']['Message'])
     else:
         return
 
